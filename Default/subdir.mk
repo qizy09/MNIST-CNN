b8e6fc7b699f24266d26a887d6d86eea007cb19d
Ziyang Qi <qizy09@gmail.com>
2015-05-10 07:51:05 -0700

    Supplementing the project

---
+++ Default/subdir.mk
@@ -0,0 +1,55 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../MNIST.o \
+../convolutionalneuralnet.o \
+../distortions.o \
+../fullyconnectedneuralnet.o \
+../layer.o \
+../main.o \
+../neuralnet.o \
+../training.o 
+
+CPP_SRCS += \
+../MNIST.cpp \
+../convolutionalneuralnet.cpp \
+../distortions.cpp \
+../fullyconnectedneuralnet.cpp \
+../layer.cpp \
+../main.cpp \
+../neuralnet.cpp \
+../training.cpp 
+
+OBJS += \
+./MNIST.o \
+./convolutionalneuralnet.o \
+./distortions.o \
+./fullyconnectedneuralnet.o \
+./layer.o \
+./main.o \
+./neuralnet.o \
+./training.o 
+
+CPP_DEPS += \
+./MNIST.d \
+./convolutionalneuralnet.d \
+./distortions.d \
+./fullyconnectedneuralnet.d \
+./layer.d \
+./main.d \
+./neuralnet.d \
+./training.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
