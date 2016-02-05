a6a6e484d64f57085e924f1704aa340e066038c7
Ziyang Qi <qizy09@gmail.com>
2016-02-05 07:30:45 -0800

    Finishing the project

--- training.cpp
+++
@@ -1,9 +0,0 @@
-#include "training.h"
-
-using std::vector;
-
-//Function to load training data from a file and then stores it in the trainingDataVector
-void loadTrainingData(std::string trainingDataFile, vector<std::tuple<float*, int*> > &trainingDataVector)
-{
-
-}
